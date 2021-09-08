14d595858ccbe5a46270ae8bc76028446be35bfe
ginnygu <ginnygu@gmail.com>
2021-09-08 11:07:46 -0400

    initial commit

---
+++ functions.js
@@ -0,0 +1,19 @@
+const identity = function (value) {
+  return value;
+};
+const first = function (array, n) {};
+
+const last = function (array, n) {};
+
+const each = function (collection, callback) {};
+const indexOf = function (array, target) {};
+const map = function (collection, iterator) {};
+
+module.exports = {
+  identity,
+  first,
+  last,
+  each,
+  indexOf,
+  map,
+};
